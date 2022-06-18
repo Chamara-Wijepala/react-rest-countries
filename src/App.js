263c39280f13f51c5dfa6ef28a53127f768a4e9f
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-18 18:52:02 +0530

    Add Header, Home and Details pages and routes to pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-function App() {
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Header from "./Pages/Header";
+import Home from "./Pages/Home";
+import Details from "./Pages/Details";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/details" element={<Details/>}></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </>
   );
 }
 
