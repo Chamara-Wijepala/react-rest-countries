8642172e5b97513201cfb0488887e5dc1f9024e2
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-18 20:53:39 +0530

    Fetch and store all countries in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Header from "./Pages/Header";
 import Home from "./Pages/Home";
 import Details from "./Pages/Details";
 
 const App = () => {
+  const [ countries, setCountries ] = useState([]);
+
+  // Fetch data of all countries after first render and store data in state
+  useEffect(() => {(
+    async () => {
+      const request = await fetch('https://restcountries.com/v3.1/all');
+      const response = await request.json()
+
+      setCountries(response);
+    }
+  )()}, [])
+
   return (
     <>
       <BrowserRouter>
@@ -11,7 +24,7 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/" element={<Home/>}></Route>
-            <Route path="/details" element={<Details/>}></Route>
+            <Route path="/details/:id" element={<Details/>}></Route>
           </Routes>
         </main>
       </BrowserRouter>
