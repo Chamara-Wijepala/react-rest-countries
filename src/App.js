ccea0bbcf2007400ddb06d64a2058d3420ded658
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-20 21:54:42 +0530

    Move useEffect hook for fecthing from App to Home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Header from "./Pages/Header";
 import Home from "./Pages/Home";
 import Details from "./Pages/Details";
-import fetchCountries from "./Helpers/fetchCountries";
 
 const App = () => {
-  const [ countries, setCountries ] = useState([]);
-
-  // Fetch data of all countries after first render and store data in state
-  useEffect(() => {(
-    async () => {
-      const response = await fetchCountries();
-
-      setCountries(response);
-    }
-  )()}, []);
-
   return (
     <>
       <BrowserRouter>
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<Home countries={countries} />}></Route>
+            <Route path="/" element={<Home/>}></Route>
             <Route path="/details/:id" element={<Details/>}></Route>
           </Routes>
         </main>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
 import RenderCountries from "../Components/RenderCountries";
+import fetchCountries from "../Helpers/fetchCountries";
 
-const Home = ({ countries }) => {
+const Home = () => {
+  const [ countries, setCountries ] = useState([]);
+
+  // Fetch data of all countries after first render and store data in state
+  useEffect(() => {(
+    async () => {
+      const response = await fetchCountries();
+
+      setCountries(response);
+    }
+  )()}, []);
+
   return (
     <>
       <div className="CountryList">
