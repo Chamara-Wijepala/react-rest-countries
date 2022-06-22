5c66be9d61535d484f6c4bc1e27d7c842433a465
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-22 16:11:33 +0530

    Add custom icon to dropdown menu

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go"
 
 const Dropdown = ({ handleClick, options }) => {
   const [ isActive, setIsActive ] = useState(false);
@@ -7,9 +8,15 @@ const Dropdown = ({ handleClick, options }) => {
   return (
     <div className="Dropdown">
 
-      <div className="DropdownBtn" onClick={() => {
-        setIsActive(!isActive);
-      }}>{selected || 'Filter By Region'}</div>
+      <div
+        className="DropdownBtn"
+        onClick={() => {
+          setIsActive(!isActive);
+        }}
+      >
+        {selected || 'Filter By Region'}
+        <GoChevronDown/>
+      </div>
 
       {isActive && (
         <div className="DropdownContent">
