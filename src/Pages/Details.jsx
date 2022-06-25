18a35913b512cd8d322a1787a9007f703484e541
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-25 17:01:35 +0530

    Create function to get all countries that border current country

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -6,14 +6,22 @@ import RenderCountryDetails from "../Components/RenderCountryDetails";
 const Details = ({ allCountries }) => {
   const { id } = useParams();
   const [ country, setCountry ] = useState(allCountries);
+  const [ borderCountries, setBorderCountries ] = useState([]);
 
-  // Filters out all countries except the country that's cca3 matches URL id
   useEffect(() => {
+    // Filters out all countries except the country that's cca3 matches URL id
     const filteredCountry = allCountries.filter((country) => {
       return country.cca3.includes(id)
     });
 
+    // Similar to the logic above, it filters out all countries except the ones
+    // that's cca3 matches the filteredCountry's bordering countries
+    const filteredBorderCountries = allCountries.filter((country) => {
+      return filteredCountry[0].borders.includes(country.cca3);
+    });
+
     setCountry(filteredCountry);
+    setBorderCountries(filteredBorderCountries);
   }, [id, allCountries]);
 
   return (
