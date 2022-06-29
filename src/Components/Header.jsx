74542df485d4e9178df335d4a34943c2d7a98b2d
Chamara-Wijepala <mr.worf777@gmail.com>
2022-06-29 20:26:43 +0530

    Add styles to Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import { StyledHeader } from "./Styles/Header.style";
+import { MdDarkMode } from 'react-icons/md';
 
 const Header = ({ toggleTheme }) => {
   return (
     <StyledHeader>
       <div className="HeaderContentWrapper">
         <h1>Where in the world?</h1>
-        <button onClick={toggleTheme}>Change Theme</button>
+        <button onClick={toggleTheme}><MdDarkMode/> Toggle Theme</button>
       </div>
     </StyledHeader>
   );
--- src/Components/Styles/Header.style.js
+++ src/Components/Styles/Header.style.js
@@ -2,4 +2,26 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.element};
+  box-shadow: 0 2px 5px rgba(0, 0, 0, .1);
+
+  .HeaderContentWrapper {
+    display: flex;
+    justify-content: space-between;
+    padding-block: 1.4rem;
+
+    h1 {
+      font-size: clamp(1rem, 4vw, 1.8rem);
+    }
+
+    button {
+      background-color: ${({ theme }) => theme.element};
+      color: ${({ theme }) => theme.text};
+      border: none;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      gap: .5rem;
+      font-size: clamp(.8rem, 4vw, 1.2rem);
+    }
+  }
 `;
