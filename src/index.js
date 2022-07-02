5ae86d6615bd3a1b1888ad6ea8e97dc55d49270c
Chamara-Wijepala <mr.worf777@gmail.com>
2022-07-02 21:26:22 +0530

    Production build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
